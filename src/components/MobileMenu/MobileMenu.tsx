b59bf53a35a9de1f6efcbbd603d95a4d4f7e3730
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-14 15:00:27 +0300

    feat: add mock data, icons, font Golos

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,10 +1,55 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import cn from 'classnames/bind';
 
 import styles from './MobileMenu.module.css';
+import data from './assets/mock-data.json';
 
 const cx = cn.bind(styles);
+const languagesList = data.map((language) => {
+  return {
+    imageLink: language.image,
+    shortName: language.shortLang,
+    fullName: language.lang,
+    id: language.id,
+  };
+});
 
 export const MobileMenu: FC = () => {
-  return <div className={cx('container')}></div>;
+  console.log(languagesList);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    languagesList.find((language) => language.shortName === 'Ru'),
+  );
+  console.log(selectedLanguage);
+
+  const handleMenuToggle = () => setIsMenuOpen(!isMenuOpen);
+  return (
+    <div className={cx('container', isMenuOpen && 'mobileMenuOpen')}>
+      <div className={cx('mobileMenuUpperLine')}>
+        <button className={cx('languageSelectButton')}>
+          <div className={cx('languageSelectIconContainer')}>
+            <img
+              className={cx('flagImage', 'languageSelectIcon')}
+              src={selectedLanguage?.imageLink || ''}
+            />
+          </div>
+          <span>{selectedLanguage?.shortName || ''}</span>
+        </button>
+        <ul className={cx('languageList')}>
+          {languagesList.map((language) => (
+            <li key={language.id}>
+              <img className={cx('flagImage')} src={language.imageLink} />
+              <span>{language.fullName}</span>
+              {language.shortName === selectedLanguage?.shortName && <span>✓</span>}
+            </li>
+          ))}
+        </ul>
+        <button
+          className={cx(isMenuOpen ? 'close' : 'burger')}
+          onClick={handleMenuToggle}
+        />
+      </div>
+      <div className={cx('mobileMenu')}></div>
+    </div>
+  );
 };
