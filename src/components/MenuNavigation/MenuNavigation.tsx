8e4dd077d2a26b8b79e317d5d9bcdd4239383519
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-15 04:26:15 +0300

    feat: add two layers to MenuNavigation

--- src/components/MenuNavigation/MenuNavigation.tsx
+++ src/components/MenuNavigation/MenuNavigation.tsx
@@ -16,34 +16,57 @@ interface IMenuNavigation {
   isMenuOpen: boolean;
 }
 
+interface ISecondLayerData {
+  id: number;
+  title: string;
+  content: { id: string; title: string; text: string }[];
+}
+
 const cx = cn.bind(styles);
 
 export const MenuNavigation: FC<IMenuNavigation> = ({ navigationData, isMenuOpen }) => {
   const [currentMenuLayer, setCurrentMenuLayer] = useState(1);
-  const [secondLayerData, setSecondLayerData] = useState();
-  const handleOpenSecondLayer = (id: number) => {
-    console.log(navigationData?.filter((el) => el.id === id));
+  const [secondLayerData, setSecondLayerData] = useState<ISecondLayerData[]>();
+  const [secondLayerTitle, setSecondLayerTitle] = useState('');
+  const handleOpenSecondLayer = (id: number, title: string) => {
+    setSecondLayerData(navigationData?.filter((el) => el.id === id)[0].content);
+    setCurrentMenuLayer(2);
+    setSecondLayerTitle(title);
   };
-
+  const handleOpenFirstLayer = () => {
+    setCurrentMenuLayer(1);
+  };
   return (
-    <div className={cx('container', isMenuOpen && 'mobileMenuOpen')}>
+    <div
+      className={cx(
+        'container',
+        currentMenuLayer === 1 && 'firstLayer',
+        isMenuOpen && 'mobileMenuOpen',
+      )}
+    >
+      {/*Первый уровень меню*/}
       <ul
         className={cx('list', 'text', 'titleLarge', currentMenuLayer !== 1 && 'hidden')}
       >
-        {navigationData?.map((firstLevelMenuElement) => (
-          <li key={firstLevelMenuElement.id}>
+        {navigationData?.map((firstLayerMenuElement) => (
+          <li key={firstLayerMenuElement.id}>
             <button
               className={cx('button', 'text', 'titleLarge')}
               type="button"
-              onClick={() => handleOpenSecondLayer(firstLevelMenuElement.id)}
+              onClick={() =>
+                handleOpenSecondLayer(
+                  firstLayerMenuElement.id,
+                  firstLayerMenuElement.title,
+                )
+              }
             >
-              {firstLevelMenuElement.title}
+              {firstLayerMenuElement.title}
             </button>
           </li>
         ))}
       </ul>
-      <nav>
-        <ul className={cx('list', currentMenuLayer !== 1 && 'hidden')}>
+      <nav className={cx(currentMenuLayer !== 1 && 'hidden')}>
+        <ul className={cx('list')}>
           <li>
             <a className={cx('link', 'text', 'titleLarge')} href="#" target="_self">
               Контакты
@@ -56,6 +79,30 @@ export const MenuNavigation: FC<IMenuNavigation> = ({ navigationData, isMenuOpen
           </li>
         </ul>
       </nav>
+      {/*Второй уровень меню*/}
+      <button
+        type="button"
+        onClick={handleOpenFirstLayer}
+        className={cx(
+          'text',
+          'button',
+          'titleLarge',
+          'titleSecondLayer',
+          'backButton',
+          currentMenuLayer !== 2 && 'hidden',
+        )}
+      >
+        {secondLayerTitle}
+      </button>
+      <ul className={cx('list', 'secondLayer', currentMenuLayer !== 2 && 'hidden')}>
+        {secondLayerData?.map((el) => (
+          <li key={el.id}>
+            <button className={cx('button', 'text', 'titleMedium')} type="button">
+              {el.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
